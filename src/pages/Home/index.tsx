f43e818c0c8e33ae1df8f8be3ede31b6a7cac018
Judson Junior <judson.junior@zema.com>
2021-05-12 09:11:07 -0300

    Modificado a estrutura das pastas
      - assets -> destinado ao armazenamento de arquivos
      - pages -> destinado para manter cada página da aplicação
      - routes -> ficará o arquivo responsável para destinar cada rota a um
        componente
      - styles -> manterá todos os estilos que será compartilhado entre os
        componentes

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,25 @@
+import logo from "assets/logo.svg";
+
+import * as S from "./styles";
+
+const Home: React.FC = () => {
+  return (
+    <S.App>
+      <S.AppHeader>
+        <S.AppLogo src={logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <S.AppLink
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </S.AppLink>
+      </S.AppHeader>
+    </S.App>
+  );
+};
+
+export default Home;
